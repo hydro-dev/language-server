cbb7d2b6734ff82746bc008c5fa2ef0fa6b1116e
undefined <i@undefined.moe>
2023-05-05 01:03:38 +0800

    force uri for java

--- pipeline.ts
+++ pipeline.ts
@@ -5,6 +5,8 @@ import * as lsp from 'vscode-languageserver';
 
 export function getPipeline(tmpFolder: string, folders = []) {
     let slashStyle = false;
+    const pendingFolder = new Set<string>();
+    let forceUri = '';
 
     function removeHydroPrefix(uri: string, replacement = '') {
         if (uri.startsWith('hydro://')) {
@@ -16,31 +18,33 @@ export function getPipeline(tmpFolder: string, folders = []) {
     }
 
     return function pipeline(message: rpc.Message) {
-        const pendingFolder = new Set<string>();
         if (rpc.isRequestMessage(message) || rpc.isNotificationMessage(message)) {
             const params = message.params as any;
             if (!params) return message;
             if (message.method === lsp.InitializeRequest.type.method) {
                 params.processId = process.pid;
             }
-            if (params.textDocument?.uri?.startsWith('hydro:')) {
+            if (params.textDocument?.uri?.endsWith('.java') && params.textDocument.text) {
+                forceUri = params.textDocument.uri;
+                fs.writeFileSync(`${tmpFolder}/Main.java`, params.textDocument.text);
+                params.textDocument.uri = `file://${tmpFolder}/Main.java`;
+            } else if (params.textDocument?.uri?.startsWith('hydro:')) {
                 pendingFolder.add(removeHydroPrefix(params.textDocument.uri));
                 params.textDocument.uri = removeHydroPrefix(params.textDocument.uri, `file://${tmpFolder}/`);
-                if (params.textDocument.uri.endsWith('Main.java') && params.textDocument.text) {
-                    fs.writeFileSync(`${tmpFolder}/Main.java`, params.textDocument.text);
-                }
             }
-            if (params.uri?.startsWith('hydro:')) {
+            if (params.uri?.endsWith('.java')) {
+                params.uri = `file://${tmpFolder}/Main.java`;
+            } else if (params.uri?.startsWith('hydro:')) {
                 pendingFolder.add(removeHydroPrefix(params.uri));
                 params.uri = removeHydroPrefix(params.uri, `file://${tmpFolder}/`);
             } else if (params.uri?.startsWith(`file://${tmpFolder}/`)) {
-                params.uri = params.uri.replace(`file://${tmpFolder}/`, slashStyle ? 'hydro://' : 'hydro:');
+                params.uri = forceUri || params.uri.replace(`file://${tmpFolder}/`, slashStyle ? 'hydro://' : 'hydro:');
             }
         } else if (rpc.isResponseMessage(message)) {
             if (message.result instanceof Array) {
                 for (const re of message.result) {
                     if (re.uri?.startsWith(`file://${tmpFolder}/`)) {
-                        re.uri = re.uri.replace(`file://${tmpFolder}/`, slashStyle ? 'hydro://' : 'hydro:');
+                        re.uri = forceUri || re.uri.replace(`file://${tmpFolder}/`, slashStyle ? 'hydro://' : 'hydro:');
                     }
                 }
             } else if (typeof message.result === 'object' && message.result !== null) {
